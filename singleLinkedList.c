fc80b7c88df9fdc4280d3d50bb3b82e1c61d4c61
amit kumar <amitkumar3968@gmail.com>
2016-04-06 15:03:08 +0530

    adding new lines.

--- singleLinkedList.c
+++ singleLinkedList.c
@@ -1,16 +1,54 @@
-#include "stdio.h"
+//
+//  main.c
+//  test
+//
+//  Created by Amit Kumar on 4/6/16.
+//  Copyright © 2016 Amit Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
 
 struct mystruct {
-int a;
-struct mystruct *node;
+    int data;
+    struct mystruct *node;
 };
 
-int main (int argc, char * argv[]) 
-{
-printf("Hello Ubuntu.\n");
+typedef struct mystruct list;
+void printList (list *);
 
-
-
-return 0;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    list *parent = (list *)malloc(sizeof(list));
+    parent -> data = 1;
+    parent -> node = NULL;
+    
+    list *child1 = (list *) malloc(sizeof(list));
+    child1 -> data = 2;
+    child1 -> node = NULL;
+    
+    parent -> node = child1;
+    
+    printList(parent);
+    return 0;
 }
+
+void printList(list *theList) {
+  
+    if (theList == NULL) {
+        printf("Empty list");
+    }else {
+        
+    list *temp = theList;
+    while (temp ) {
+        printf("\n %d \n",temp ->data);
+        temp = temp -> node;
+        
+    }
+    }
+};
+
+
+
 
